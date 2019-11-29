6c122f8394151ca525582e4104ded3dd42e51d02
budfy <serdyuk.ro@gmail.com>
2019-11-29 15:31:15 +0200

    29.11.2019

---
+++ gulpfile.js
@@ -0,0 +1,184 @@
+'use strict';
+
+//определим переменные для функцийи плагинов
+let gulp = require ('gulp'),
+    sass = require('gulp-sass'), //препроцессор
+    cssmin = require('gulp-cssmin'), //минификатор CSS
+    prefixer = require('gulp-autoprefixer'), //автоматическая расстановка вендорных префиксов
+    include = require('gulp-file-include'), //импорт одних файлов в другие (работает с HTML, SCSS/CSS и JS, но нужен он нам в основном для импорта HTML)
+    browserSync = require('browser-sync'), //сервер для отображения в браузере в режиме реального времени
+    rename = require('gulp-rename'), //переименовывает файлы, добавляет им префиксы и суффиксы
+    imagemin = require('gulp-imagemin'), //пережимает изображения
+    recompress = require('imagemin-jpeg-recompress'), //тоже пережимает, но лучше. Плагин для плагина
+    uglify = require('gulp-uglify'), //то же, что cssmin, только для js
+    concat = require('gulp-concat'), //склеивает css и js-файлы в один
+    del = require('del'), //удаляет указанные файлы и директории. Нужен для очистки перед билдом.
+    sourcemaps = require('gulp-sourcemaps'); //рисует карту слитого воедино файла, чтобы было понятно, что из какого файла бралось
+    //favgen = require('gulp-favicons'); //создаём иконки под все типы устройств
+
+    gulp.task('scss', function(){ //делаем из своего scss-кода css для браузера
+     return gulp.src('src/scss/**/*.scss') //берём все файлы в директории scss и директорий нижнего уровня
+     .pipe(sourcemaps.init()) //инициализируем sourcemaps, чтобы он начинал записывать, что из какого файла берётся
+     .pipe(sass({outputStyle:'compressed'})) //конвертируем scss в css и импортируем все импорты
+     .pipe(rename({suffix: '.min'})) //переименовываем файл, чтобы было понятно, что он минифицирован
+     .pipe(prefixer({ //добавляем вендорные префиксы
+       overrideBrowserslist: ['last 22 versions'], //последние 8 версий, но можно донастроить на большее или меньшее значение
+       browsers: [
+        "Android >= 4",
+        "Chrome >= 20",
+        "Firefox >= 24",
+        "Explorer >= 9",
+        "iOS >= 6",
+        "Opera >= 12",
+        "Safari >= 6"
+    ]
+      }))
+     .pipe(sourcemaps.write()) //записываем карту в итоговый файл
+     .pipe(gulp.dest('build/css')) //кладём итоговый файл в директорию build/css
+     .pipe(browserSync.reload({stream:true})) //обновляем браузер
+    });
+
+    //Далее будут похожие или полностью аналогичные функции, которые нет смысла расписывать. Смотрите по аналогии с вышеописанными.
+
+    gulp.task('style', function(){ //создаём единую библиотеку из css-стилей всех плагинов
+      return gulp.src([ //указываем, где брать исходники
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/animate.css/animate.css'
+      ])
+      .pipe(sourcemaps.init())
+      .pipe(concat('libs.min.css')) //склеиваем их в один файл с указанным именем
+      .pipe(cssmin()) //минифицируем полученный файл
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('build/css')) //кидаем готовый файл в директорию
+    });
+
+    
+
+    gulp.task('script', function(){ //аналогично поступаем с js-файлами
+      return gulp.src([ //тут подключаем разные js в общую библиотеку. Отключите то, что вам не нужно.
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'src/js/jquery.resizeOnApproach.1.0.min.js',
+        'node_modules/isotope-layout/dist/isotope.pkgd.js',
+        'node_modules/wowjs/dist/wow.js',
+        'node_modules/jquery-scrollify-vilcheuski/jquery.scrollify.js',
+      ])
+      .pipe(sourcemaps.init())
+      .pipe(concat('libs.min.js'))
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('build/js'))
+    });
+
+    gulp.task('minjs', function(){ //минифицируем наш main.js и перекидываем в директорию build
+      return gulp.src(['src/js/main.js'])
+      .pipe(uglify())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('build/js')) 
+    });
+
+    gulp.task('js', function(){ //обновляем браузер, если в наших js файлах что-то поменялось
+      return gulp.src('src/js/**/*.js')
+      .pipe(browserSync.reload({stream: true})) 
+    });
+    
+    gulp.task('html', function(){ //собираем html из кусочков
+      return gulp.src(['src/**/*.html', '!src/components/**/*.html'])
+      .pipe(sourcemaps.init())
+     .pipe(include({ //импортируем файлы с префиксом @@. ПРефикс можно настроить под себя.
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/'))
+    .pipe(browserSync.reload({stream:true}));
+    });
+
+    gulp.task('fonts', function(){ //перекидываем шрифты из директории src в build, а заодно следим за новыми файлами, чтобы обновлять браузер, когда появляется шрифт
+      return gulp.src('src/fonts/**/*.+(eot|svg|ttf|woff|woff2)')
+      .pipe(gulp.dest('build/fonts'))
+      .pipe(browserSync.reload({stream:true}));
+    });
+
+    // gulp.task('favicons', function(){ //генератор favicon для всех устройств. Запускается вручную отдельной командой. Генерирует фавиконки на все случаи жизни и файл favicons.html, в котором находятся подключения этих иконок. Скопируйте подключения в файлы проекта и удалите favicons.html Больше нужно для веб-приложений, потому что их ярлыки выносят на главный экран. Сайтам же достаточно закинуть и подключить одну favicon.ico Короче, если вы не уверены, что большинство пользователей мобильных устройств запихнут ярлык вашего сайта на главный экран и разрешат push-уведомления в телефоне, ваша фамилия не Цукерберг и не Дуров - вам этот таск, скорее всего не нужен.
+    //   return gulp.src('src/img/favicon/favicon.png')
+    //   .pipe(favgen({
+    //     appName: 'My App',
+    //     appShortName: 'App',
+    //     appDescription: 'This is my application',
+    //     developerName: 'Hayden Bleasel',
+    //     developerURL: 'http://haydenbleasel.com/',
+    //     background: '#020307',
+    //     path: 'favicons/',
+    //     url: 'http://haydenbleasel.com/',
+    //     display: 'standalone',
+    //     orientation: 'portrait',
+    //     scope: '/',
+    //     start_url: '/?homescreen=1',
+    //     version: 1.0,
+    //     logging: false,
+    //     html: 'favicons.html',
+    //     pipeHTML: true,
+    //     replace: true,
+    //   })
+    //   )
+    //   .pipe(gulp.dest('src/'));
+    // });
+
+    gulp.task('images', function(){ //пережимаем изображения и складываем их в директорию build
+      return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg|ico)')
+      .pipe(imagemin([
+        recompress({ //Настройки сжатия изображений. Сейчас всё настроено так, что сжатие почти незаметно для глаза на обычных экранах. Можете покрутить настройки, но за результат не отвечаю.
+          loops: 4, //количество прогонок изображения
+          min: 70, //минимальное качество в процентах
+          max: 80, //максимальное качество в процентах
+          quality: 'high' //тут всё говорит само за себя, если хоть капельку понимаешь английский
+        }),
+        imagemin.gifsicle(), //тут и ниже всякие плагины для обработки разных типов изображений
+        imagemin.optipng(),
+        imagemin.svgo()
+      ]))
+      .pipe(gulp.dest('build/img'))
+      .pipe(browserSync.reload({stream:true}));
+    });
+
+    gulp.task('deletefonts', function() { //задачи для очистки директории со шрифтами в build. Нужна для того, чтобы удалить лишнее.
+      return del('build/fonts/**/*.*');
+    });
+
+    gulp.task('deleteimg', function() { //аналогично предыдущей, но с картинками.
+      return del('build/img/**/*.*');
+    });
+
+    gulp.task('cleanfonts', gulp.series('deletefonts', 'fonts')); //задачи нужна для того, чтобы сразу очистить директорию и залить шрифты по-новой
+
+    gulp.task('cleanimg', gulp.series('deleteimg', 'images')); //задачи нужна для того, чтобы сразу очистить директорию и залить картинки по-новой
+    
+
+
+    gulp.task('watch', function(){ //Следим за изменениями в файлах и директориях и запускаем задачи, если эти изменения произошли
+      gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
+      gulp.watch('src/**/*.html', gulp.parallel('html'));
+      gulp.watch('src/fonts/**/*.*', gulp.parallel('fonts'));
+      gulp.watch('src/js/**/*.js', gulp.parallel('minjs', 'js'));
+      gulp.watch('src/img/**/*.*', gulp.parallel('images'));
+    });
+
+
+
+    gulp.task('browser-sync', function() { //настройки лайв-сервера
+      browserSync.init({
+          server: {
+              baseDir: "build/" //какую папку показывать в браузере
+          },
+          browser: ["chrome"], //в каком браузере
+          //tunnel: "z-union", //тут можно прописать название проекта и дать доступ к нему через интернет. Работает нестабильно, запускается через раз. Не рекомендуется включать без необходимости.
+          //tunnel:true, //работает, как и предыдущяя опция, но присваивает рандомное имя. Тоже запускается через раз и поэтому не рекомендуется для включения
+          //host: "192.168.0.103" //IP сервера в локальной сети. Отключите, если у вас DHCP, пропишите под себя, если фиксированный IP в локалке.
+      });
+    });
+
+    gulp.task('default', gulp.parallel('browser-sync', 'watch', 'scss', 'style', 'script', 'minjs', 'html', 'cleanfonts', 'cleanimg')) //запускает все перечисленные задачи разом
---
+++ src/js/main.js
@@ -0,0 +1,502 @@
+$(function () {
+
+  $(window).scroll(function() {
+    var url = window.location.hash;
+    if($(this).scrollTop() >= window.innerHeight * 0.8 && url != '#8') {
+        $('.top__menu').addClass('scrolled');
+    }
+    else{
+        $('.top__menu').removeClass('scrolled');
+    }
+  });
+
+  $('.burger__menu, .burger__menu-black').click(function () {
+    $('.header__nav').toggleClass('header__nav--open');
+  });
+
+  $('.cases__burger-icon').click(function () {
+    $(this).toggleClass('clicked');
+    $('.cases__nav').toggleClass('cases__nav--open');
+  });
+
+  $('.technologies__burger-icon').click(function () {
+    $(this).toggleClass('clicked');
+    $('.technologies__nav').toggleClass('technologies__nav--open');
+  });
+
+  $('.team__inner img').click(function () {
+    $('.team__inner').toggleClass('team__inner--clicked');
+  });
+
+  //------------------------------------- popups controlling-----------------------
+
+  $('.first-popup__link').click(function () {
+    $('#first-popup').addClass('popup--open');
+  });
+  $('.second-popup__link').click(function () {
+    $('#second-popup').addClass('popup--open');
+  });
+  $('.third-popup__link').click(function () {
+    $('#third-popup').addClass('popup--open');
+  });
+  $('.fourth-popup__link').click(function () {
+    $('#fourth-popup').addClass('popup--open');
+  });
+  $('.fifth-popup__link').click(function () {
+    $('#fifth-popup').addClass('popup--open');
+  });
+  $('.sixth-popup__link').click(function () {
+    $('#sixth-popup').addClass('popup--open');
+  });
+  $('.seventh-popup__link').click(function () {
+    $('#seventh-popup').addClass('popup--open');
+  });
+  $('.eight-popup__link').click(function () {
+    $('#eight-popup').addClass('popup--open');
+  });
+  $('.nineth-popup__link').click(function () {
+    $('#nineth-popup').addClass('popup--open');
+  });
+  $('.tenth-popup__link').click(function () {
+    $('#tenth-popup').addClass('popup--open');
+  });
+  $('.eleventh-popup__link').click(function () {
+    $('#eleventh-popup').addClass('popup--open');
+  });
+  $('.tvelveth-popup__link').click(function () {
+    $('#tvelveth-popup').addClass('popup--open');
+  });
+  $('.therteentnth-popup__link').click(function () {
+    $('#therteentnth-popup').addClass('popup--open');
+  });
+  $('.fourteenth-popup__link').click(function () {
+    $('#fourteenth-popup').addClass('popup--open');
+  });
+
+  $('.edu__link').click(function (){
+    $('.popup__tabs').find('.popup__tab--open').removeClass('popup__tab--open');
+    $('.popup__tab-menu').find('.popup__tab-link').removeClass('popup__tab-link--open');
+    $('.popup__tab-edu').addClass('popup__tab--open');
+    $('.popup__edu-link').addClass('popup__tab-link--open');
+  });
+
+  $('.exp__link').click(function (){
+    $('.popup__tabs').find('.popup__tab--open').removeClass('popup__tab--open');
+    $('.popup__tab-menu').find('.popup__tab-link').removeClass('popup__tab-link--open');
+    $('.popup__tab-exp').addClass('popup__tab--open');
+    $('.popup__exp-link').addClass('popup__tab-link--open');
+  });
+
+  $('.skill__link').click(function (){
+    $('.popup__tabs').find('.popup__tab--open').removeClass('popup__tab--open');
+    $('.popup__tab-menu').find('.popup__tab-link').removeClass('popup__tab-link--open');
+    $('.popup__tab-skill').addClass('popup__tab--open');
+    $('.popup__skill-link').addClass('popup__tab-link--open');
+  });
+
+  $('.popup__close-btn').click(function(){
+    $('.popup__block').find('.popup--open').removeClass('popup--open')
+  });
+
+  $('.popup__edu-link').click(function(){
+    $('.popup__tab-menu').find('.popup__tab-link').removeClass('popup__tab-link--open');
+    $('.popup__tabs').find('.popup__tab--open').removeClass('popup__tab--open');
+    $('.popup__tab-edu').addClass('popup__tab--open');
+    $('.popup__edu-link').addClass('popup__tab-link--open');
+  });
+  $('.popup__exp-link').click(function(){
+    $('.popup__tab-menu').find('.popup__tab-link').removeClass('popup__tab-link--open');
+    $('.popup__tabs').find('.popup__tab--open').removeClass('popup__tab--open');
+    $('.popup__tab-exp').addClass('popup__tab--open');
+    $('.popup__exp-link').addClass('popup__tab-link--open');
+  });
+  $('.popup__skill-link').click(function(){
+    $('.popup__tab-menu').find('.popup__tab-link').removeClass('popup__tab-link--open');
+    $('.popup__tabs').find('.popup__tab--open').removeClass('popup__tab--open');
+    $('.popup__tab-skill').addClass('popup__tab--open');
+    $('.popup__skill-link').addClass('popup__tab-link--open');
+  });
+
+   //------------------------------------- popups controlling-----------------------
+
+  $.scrollify({
+    section: ".slide-section",
+    easing: "easeOutExpo",
+    scrollSpeed: 1200,
+    offset: 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll: true,
+    before: function () {},
+    after: function () {},
+    afterResize: function () {},
+    afterRender: function () {}
+  });
+
+  $('.nav__link').each(function () {
+    var location = window.location.pathname;
+    var link = this.pathname;
+    if (location == link) {
+      $(this).addClass('active');
+    }
+  });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 77,
+    loop: false,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      },
+      1440: {
+        slidesPerView: 5,
+        spaceBetween: 77
+      }
+    },
+  });
+
+  $('.tab__link').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.tab__content').find('.tab__content-item').removeClass('current-tab').hide();
+    $('.tab__nav').find('.tab__link').removeClass('tab__link-current');
+    $(this).addClass('tab__link-current');
+    $(id).addClass('current-tab').fadeIn();
+    $('.cases__nav').removeClass('cases__nav--open');
+    $('.technologies__nav').removeClass('technologies__nav--open');
+    return false;
+  });
+
+  $('#slider-one>.slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: false,
+    variableWidth: true,
+    rows: "0",
+    appendArrows: $('#slider-one'),
+    responsive: [{
+      breakpoint: 651,
+      settings: {
+        arrows: false
+      }
+    }, ]
+  });
+
+  $('#slider-two>.slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: false,
+    variableWidth: true,
+    rows: "0",
+    appendArrows: $('#slider-two'),
+    responsive: [{
+      breakpoint: 651,
+      settings: {
+        arrows: false
+      }
+    }, ]
+  });
+
+  $('#slider-three>.slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: false,
+    variableWidth: true,
+    rows: "0",
+    appendArrows: $('#slider-three'),
+    responsive: [{
+      breakpoint: 651,
+      settings: {
+        arrows: false
+      }
+    }, ]
+  });
+
+  $('#slider-four>.slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: false,
+    variableWidth: true,
+    rows: "0",
+    appendArrows: $('#slider-four'),
+    responsive: [{
+      breakpoint: 651,
+      settings: {
+        arrows: false
+      }
+    }, ]
+  });
+
+  $('.presentation__inner').slick({
+    centerMode: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    variableWidth: true,
+    touchMove: true,
+    swipeToSlide: true,
+    focusOnSelect: true,
+    edgeFriction: 0.5,
+    rows: "0",
+    dots: true,
+    appendDots: $('.presentation__inner'),
+    appendArrows: $('.presentation__inner'),
+  });
+
+  // ----------------------------------------------------- tab slider ---------------------------
+  $('.tabs__carousel').slick({
+    infinite: false,
+    variableWidth: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    rows: '0',
+    touchMove: true,
+    swipeToSlide: true,
+    focusOnSelect: true,
+    edgeFriction: 0.5
+  });
+  //----------------------------------------------------- tab slider ---------------------------
+
+  $('.clients__logos-mobile').slick({
+    infinite: false,
+    arrows: false,
+    variableWidth: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    rows: '0',
+    touchMove: true,
+    swipeToSlide: true,
+    focusOnSelect: true,
+    edgeFriction: 0.5
+  });
+
+  var $container = $('.blog__content');
+  // sorting
+  $('#blog__sort-box button').click(function () {
+    // get href attribute, minus the #
+    var $this = $(this);
+    sortName = $this.attr('data-sort-by');
+    asc = $this.parents('.sort').hasClass('asc');
+    $container.isotope({
+      sortBy: sortName,
+      sortAscending: asc
+    });
+    return false;
+  });
+
+  $(function () {
+
+    $container.isotope({
+      itemSelector: '.blog__topic',
+      getSortData: {
+        symbol: '[data-postdate]',
+        number: '.topic__views parseInt',
+      }
+    });
+  });
+  $('.sort__btn').click(function () {
+    $(this).removeClass('sort__btn--on');
+    $(this).siblings().addClass('sort__btn--on');
+  });
+
+  wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  })
+  wow.init();
+
+  var position = [55.989973, 37.230520];
+
+  function showGoogleMaps() {
+    var latLng = new google.maps.LatLng(position[0], position[1]);
+    var mapOptions = {
+      zoom: 15,
+      streetViewControl: false,
+      scaleControl: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: latLng,
+      styles: [{
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#f5f5f5"
+          }]
+        },
+        {
+          "elementType": "labels.icon",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#616161"
+          }]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [{
+            "color": "#f5f5f5"
+          }]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#bdbdbd"
+          }]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#eeeeee"
+          }]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#757575"
+          }]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#e5e5e5"
+          }]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#9e9e9e"
+          }]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#ffffff"
+          }]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#757575"
+          }]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#dadada"
+          }]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#616161"
+          }]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#9e9e9e"
+          }]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#e5e5e5"
+          }]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#eeeeee"
+          }]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#c9c9c9"
+          }]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#9e9e9e"
+          }]
+        }
+      ]
+    };
+
+
+    map = new google.maps.Map(document.getElementById('contacts__map'),
+      mapOptions);
+
+    // Show the default red marker at the location
+    marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.DROP
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+
+  $('.contact-form__input-label>input').on('keydown keypress keyup paste change input', function () {
+    if ($("#input-name").val() != "") {
+      $('#wrapper-name').addClass('wrapper-filled');
+    } else {
+      $('#wrapper-name').removeClass('wrapper-filled');
+    }
+    if ($("#input-phone").val() != "") {
+      $('#wrapper-phone').addClass('wrapper-filled');
+    } else {
+      $('#wrapper-phone').removeClass('wrapper-filled');
+    }
+    if ($("#input-text").val() != "") {
+      $('#wrapper-text').addClass('wrapper-filled');
+    } else {
+      $('#wrapper-text').removeClass('wrapper-filled');
+    }
+  });
+
+  $('.presentation__slide[aria-hidden="true"]').addClass('someclass');
+
+});
